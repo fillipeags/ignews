527a401f2ceaa464312aedc9eee32a40fa9966f2
Fillipe <fillipe484@gmail.com>
2021-05-09 23:06:44 -0300

    finish basic version using ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
-import {GetServerSideProps} from 'next';
+import {GetStaticProps} from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
+
+// Client-side
+// Server-side
+// Static Site Generation
 
 interface HomeProps {
   product: {
@@ -35,7 +39,7 @@ export default function Home({product} : HomeProps) {
   )
 }
 
-export const getServerSideProps : GetServerSideProps = async () => {
+export const getStaticProps : GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IpNe2DxS0PiIHT70PltyAi4')
 
   const product = {
@@ -49,6 +53,7 @@ export const getServerSideProps : GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
